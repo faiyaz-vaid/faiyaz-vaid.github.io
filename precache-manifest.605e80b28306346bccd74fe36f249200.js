529eecf667cb54dbdc4a4daa954cc6f26a7ee573
faiyaz-vaid <faiyaz.vaid786@gmail.com>
2021-03-04 21:26:08 +0530

    Updates

---
+++ precache-manifest.605e80b28306346bccd74fe36f249200.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff56fb4cfaa878050e54ce5d9727fda",
+    "url": "/faiyaz-vaid.github.io/index.html"
+  },
+  {
+    "revision": "1b88ce981ad497857c1f",
+    "url": "/faiyaz-vaid.github.io/static/css/2.b226b99e.chunk.css"
+  },
+  {
+    "revision": "6db8156674ec83abe589",
+    "url": "/faiyaz-vaid.github.io/static/css/main.7d43ab7b.chunk.css"
+  },
+  {
+    "revision": "1b88ce981ad497857c1f",
+    "url": "/faiyaz-vaid.github.io/static/js/2.61ac8c38.chunk.js"
+  },
+  {
+    "revision": "a744c05c0a3e0380360aa8a90a843793",
+    "url": "/faiyaz-vaid.github.io/static/js/2.61ac8c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db8156674ec83abe589",
+    "url": "/faiyaz-vaid.github.io/static/js/main.7c50e326.chunk.js"
+  },
+  {
+    "revision": "abf98dfadd03e0b2235c",
+    "url": "/faiyaz-vaid.github.io/static/js/runtime-main.430ce3db.js"
+  },
+  {
+    "revision": "141fb2075cdcbe068b21dfc6d1e67556",
+    "url": "/faiyaz-vaid.github.io/static/media/3.141fb207.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/faiyaz-vaid.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/faiyaz-vaid.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/faiyaz-vaid.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/faiyaz-vaid.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/faiyaz-vaid.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cf5ae32f92e21cfcc7ae38441ca312f0",
+    "url": "/faiyaz-vaid.github.io/static/media/me-1.cf5ae32f.jpg"
+  },
+  {
+    "revision": "b31f287889157256c482d631e012741b",
+    "url": "/faiyaz-vaid.github.io/static/media/me.b31f2878.jpg"
+  }
+]);
